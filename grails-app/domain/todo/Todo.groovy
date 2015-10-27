8c9928257006e51e0a0eb6e4f070d6d95143706b
ligun <choonggeun@gmail.com>
2015-10-27 17:59:33 +0900

    ハンズオン資料に従い記述

---
+++ grails-app/domain/todo/Todo.groovy
@@ -0,0 +1,9 @@
+package todo
+
+class Todo {
+    String content
+
+    static constraints = {
+        content blank: false, maxSize: 20
+    }
+}
--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,15 @@
+import todo.Todo
+
 class BootStrap {
 
     def init = { servletContext ->
+        environments {
+            development {
+                new Todo(content: "ほげほげ").save()
+                new Todo(content: "ふがふが").save()
+                new Todo(content: "ぴよぴよ").save()
+            }
+        }
     }
     def destroy = {
     }
